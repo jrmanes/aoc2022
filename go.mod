cb0343c6ba73e9c7894cc27b69269c72daf56696
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-12-08 21:17:57 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jrmanes/aoc2022/01
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// part1 returns the biggest in the file input.txt
+func part1() {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	//Search for the maximum sum of calories
+	maxCalories := 0
+	currentCalories := 0
+
+	for fileScanner.Scan() {
+		num := fileScanner.Text()
+		t, err := strconv.Atoi(num)
+
+		//
+		currentCalories += t
+
+		if err != nil {
+			if currentCalories > maxCalories {
+				maxCalories = currentCalories
+			}
+			//I start with a new count
+			currentCalories = 0
+		}
+	}
+	fmt.Println(maxCalories)
+}
+
+// part2
+func part2() {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	maxCalories1 := 0
+	maxCalories2 := 0
+	maxCalories3 := 0
+	currentCalories := 0
+
+	for fileScanner.Scan() {
+		num := fileScanner.Text()
+		t, err := strconv.Atoi(num)
+
+		currentCalories += t
+
+		if err != nil {
+			if currentCalories > maxCalories3 {
+				maxCalories3 = currentCalories
+			}
+			if maxCalories3 > maxCalories2 {
+				maxCalories3, maxCalories2 = maxCalories2, maxCalories3
+			}
+			if maxCalories2 > maxCalories1 {
+				maxCalories2, maxCalories1 = maxCalories1, maxCalories2
+			}
+
+			//I start with a new count
+			currentCalories = 0
+		}
+	}
+	fmt.Println(maxCalories1 + maxCalories2 + maxCalories3)
+}
+
+func main() {
+	fmt.Println("Part 1")
+	part1()
+	fmt.Println("Part 2")
+	part2()
+}
