b19be0870c6963f8548e5b46be3c245e999b022c
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-12-10 11:50:30 +0100

    Add day 4

--- 04/main.go
+++ 04/main.go
@@ -1,25 +1,72 @@
 package main
 
 import (
-	"bufio"
+	_ "embed"
 	"fmt"
-	"os"
+	"strconv"
+	"strings"
+	"text/scanner"
+	"time"
 )
 
-// https://adventofcode.com/2022/day/4
-func part1() {
-	readFile, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer readFile.Close()
-	sc := bufio.NewScanner(readFile)
+//go:embed input.txt
+var input_day string
 
-	for sc.Scan() {
-		fmt.Println(sc.Text())
+func Part1(input string) int {
+	var s scanner.Scanner
+	s.Init(strings.NewReader(input))
+	s.Filename = "example"
+	s.Mode ^= scanner.SkipComments  // don't skip comments
+	s.Whitespace |= 1<<'-' | 1<<',' // skip minus and comma
+
+	res := 0
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		a, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip -
+		s.Scan()
+		b, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip ,
+		s.Scan()
+		c, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip -
+		s.Scan()
+		d, _ := strconv.Atoi(s.TokenText())
+		// check inclusion
+		if (a <= c && d <= b) || (c <= a && b <= d) {
+			res++
+		}
 	}
+	return res
 }
 
+func Part2(input string) int {
+	var s scanner.Scanner
+	s.Init(strings.NewReader(input))
+	s.Whitespace |= 1<<'-' | 1<<',' // skip minus and comma
+	res := 0
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		a, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		b, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		c, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		d, _ := strconv.Atoi(s.TokenText())
+		// check overlap
+		if !(b < c || d < a) {
+			res++
+		}
+	}
+	return res
+}
+
+// https://adventofcode.com/2022/day/4
 func main() {
-	part1()
+	start := time.Now()
+	fmt.Println("part1: ", Part1(input_day))
+	fmt.Println(time.Since(start))
+
+	start2 := time.Now()
+	fmt.Println("part2: ", Part2(input_day))
+	fmt.Println(time.Since(start2))
 }
